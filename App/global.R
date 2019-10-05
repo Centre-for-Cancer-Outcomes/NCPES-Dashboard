c462f78dc96ad54fcdc6a5dcf1cd987be5a329df
Cancer <david.egan@nhs.net>
2019-10-05 20:34:45 +0100

    chage of data source

--- App/global.R
+++ App/global.R
@@ -7,10 +7,10 @@ library(DT)
 library(dqshiny)
 
 ##load in data set for app
-ncpes<-read.csv("C:/Users/DEGAN001/Documents/GIT clones/COSD_Level2 App/NCPES Dashboard/CPESDataset.csv",sep = ",", na.strings = "NA",
+#ncpes<-read.csv("C:/Users/DEGAN001/Documents/GIT clones/COSD_Level2 App/NCPES Dashboard/CPESDataset.csv",sep = ",", na.strings = "NA",
+ #               stringsAsFactors = FALSE)
+ncpes<-read.csv("https://raw.githubusercontent.com/Centre-for-Cancer-Outcomes/NCPES-Dashboard/master/CPESDataset.csv",sep = ",", na.strings = "NA",
                 stringsAsFactors = FALSE)
-#ncpes<-read.csv("https://raw.githubusercontent.com/Centre-for-Cancer-Outcomes/NCPES-Dashboard/master/CPESDataset.csv",sep = ",", na.strings = "NA",
-#                stringsAsFactors = FALSE)
 ncpes$qnum <- as.numeric(gsub("[a-zA-Z ]", "", ncpes$Question.Number))
 ncpes$scored.percentage <-  ifelse(ncpes$scored.percentage > 1,ncpes$scored.percentage,ncpes$scored.percentage * 100)
 ncpes$Lower.95..confidence.interval <-ifelse(ncpes$Lower.95..confidence.interval >1,ncpes$Lower.95..confidence.interval,ncpes$Lower.95..confidence.interval *100)
@@ -25,10 +25,10 @@ ncpes$abrvperformance <- ifelse(ncpes$Performance.Rating == 1, "Above",
 
 ncpes$Number.of.responses <- as.numeric(gsub(",","",ncpes$Number.of.responses))
 
-#regionlookup <- read.csv("https://raw.githubusercontent.com/Centre-for-Cancer-Outcomes/NCPES-Dashboard/master/CPES_geog_lookuptable.csv",
-#                         sep=",",header = TRUE)
-regionlookup<-read.csv("C:/Users/DEGAN001/Documents/GIT clones/COSD_Level2 App/NCPES Dashboard/CPES_geog_lookuptable.csv",sep = ",", na.strings = "NA",
-                stringsAsFactors = FALSE)
+regionlookup <- read.csv("https://raw.githubusercontent.com/Centre-for-Cancer-Outcomes/NCPES-Dashboard/master/CPES_geog_lookuptable.csv",
+                         sep=",",header = TRUE)
+#regionlookup<-read.csv("C:/Users/DEGAN001/Documents/GIT clones/COSD_Level2 App/NCPES Dashboard/CPES_geog_lookuptable.csv",sep = ",", na.strings = "NA",
+#               stringsAsFactors = FALSE)
 regionlookup$CALNCV18NM <- trimws(regionlookup$CALNCV18NM)
 
 ncpes <- left_join(ncpes,select(regionlookup,CCG18CDH,CALNCV18NM), by = c("Trust.Code" ="CCG18CDH" ))
